32213f8b4679378c7f43ea745a611b30e4ca8421
jackcarey <35701383+jackcarey@users.noreply.github.com>
2021-02-05 14:49:29 +0000

    finished this weeks tutorial

--- BasicFilmDbProject/Controllers/HomeController.cs
+++ BasicFilmDbProject/Controllers/HomeController.cs
@@ -14,18 +14,86 @@ namespace BasicFilmDbProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult OneFilm()
+
+        {
+
+            Film model = _context.Films.FirstOrDefault();
+            return View(model);
+
+        }
+
+        public IActionResult AllFilms()
+
+        {
+
+            var films12Only = from f in _context.Films
+                              where f.FilmCertificate == "12"
+                              select f;
+
+            bool is12Only = false;
+
+            List<Film> model = is12Only ? films12Only.ToList() : _context.Films.ToList();
+
+            return View(model);
+
+        }
+
+        public IActionResult FilmDetails(int id)
+
+        {
+
+            Film model = _context.Films.Find(id);
+
+            return View(model);
+
+        }
+
+        public IActionResult Search(String SearchString)
+
+        {
+
+            if (!string.IsNullOrEmpty(SearchString))
+
+            {
+
+
+                var films = from m in _context.Films
+
+                            where m.FilmTitle.Contains(SearchString)
+
+                            select m;
+
+                List<Film> model = films.ToList();
+
+                ViewData["SearchString"] = SearchString;
+
+                return View(model);
+
+            }
+            else
+            {
+                return View();
+            }
+
+
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
